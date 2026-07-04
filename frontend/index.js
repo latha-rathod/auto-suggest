6dccefee113f8ceb0c6ead972d26b6527200c814
Latha-rathod <kethaavathlatha2024@gmail.com>
2026-07-04 11:15:03 +0530

    5th commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,28 +1,35 @@
 const users = [
-  {
-    "name" :"Jane Doe",
-    "gender" : "Female",
-    "image" : "../images/jane.png"
-  },
-  {
-    "name" :"John Doe",
-    "gender" : "Male",
-    "image" : "../images/john.png"
-  },
-]
-let curUserId = 0;
-function toggle(){
-  if(curUserId == 0)
-    curUserId=1;
-  else
-    curUserId=0;
-  // image
-  document.getElementById("img").src=users[curUserId].image;
-  // name
-  document.getElementById("card-name").innerText=users[curUserId].name;
-  document.getElementById("card-gender").innerText=users[curUserId].gender;
+    {
+        name: "Jane Doe",
+        gender: "Female",
+        image: "./images/jane.png"
+    },
+    {
+        name: "John Doe",
+        gender: "Male",
+        image: "./images/john.png"
+    }
+];
 
+let curUserId = 0;
+
+function toggle() {
+    //console.log("frontend");
+    //gets printed browser console
+
+    curUserId = (curUserId + 1) % users.length;
+
+    document.getElementById("img").src = users[curUserId].image;
+
+    document.getElementById("card-name").innerText =
+        users[curUserId].name;
+
+    document.getElementById("card-gender").innerText =
+        users[curUserId].gender;
 }
+
+console.log("Hello from JavaScript");
+
 function getRandomUser()
 {
   fetch("https://randomuser.me/api")
